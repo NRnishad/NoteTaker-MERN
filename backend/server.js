0a29aace172d5936c70c65482ad57a378382c974
NRnishad <nrnishad106@gmail.com>
2025-06-18 23:03:00 +0530

    script updated

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express();
 app.get('/api/test', (req, res) => {
-  res.status(200).send('Hello, World!');        
+  res.status(200).send('Hello, World! This is a test endpoint.');        
 });
 
 app.listen(3000, () => {
