011868df7543767eb28604205cb4bfa27d94b340
NRnishad <nrnishad106@gmail.com>
2025-06-19 06:01:36 +0530

    routs folder added

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
+import notesRoutes from './routes/notesRoutes.js';
 const app = express();
-app.get('/api/test', (req, res) => {
-  res.status(200).send('Hello, World! This is a test endpoint.');        
-});
+
+app.use('/api/notes', notesRoutes)
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
