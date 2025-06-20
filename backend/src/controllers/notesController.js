b3b452f810b099b3a0091c61f7698f56a5f349e9
NRnishad <nrnishad106@gmail.com>
2025-06-20 06:42:09 +0530

    create first controller for get notes

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,5 +1,13 @@
-export const getNotes = (req, res) => {
-    res.status(200).send('This is the notes route');
+import Note from '../models/Note.js';
+
+export const getNotes =async (req, res) => {
+   try{
+    const notes = await Note.find();
+    res.status(200).json(notes);
+   }catch(error) {
+    console.error(error);
+       res.status(500).json({massage:'internal server error'});
+   }
 }
 export const createNote = (req, res) => {
     res.status(201).send('Note created successfully');
