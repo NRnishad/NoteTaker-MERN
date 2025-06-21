7820a5ab6f754d0145e3223837cdef43700269dd
NRnishad <nrnishad106@gmail.com>
2025-06-21 12:04:24 +0530

    route added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Routes,Route } from 'react-router'
+import HomePage from './pages/HomePage.jsx'
+import CreatePage from './pages/CreatePage.jsx'
+import NoteDetailPage from './pages/NoteDetailPage.jsx'
+import './App.css'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/create" element={<CreatePage />} />
+      <Route path="/note/:id" element={<NoteDetailPage />} />
+      <Route path="*" element={<div>404 Not Found</div>} />
+    </Routes>
+      </div>
   )
 }
 
